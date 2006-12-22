1312d805e5d7d85c78f6aa4c9218524987d16986
boemann <boemann@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-12-22 16:04:35 +0000

    Fix the layout
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/playground/artwork@615776 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- Oxygen/style_and_windec/windec/code/oxygenclient.cpp
+++ Oxygen/style_and_windec/windec/code/oxygenclient.cpp
@@ -330,26 +330,27 @@ void OxygenClient::init()
                                 QSizePolicy::Fixed);
 
     mainlayout->addItem(new QSpacerItem(LFRAMESIZE, TFRAMESIZE), 0, 0);
-    mainlayout->addItem(new QSpacerItem(0, TFRAMESIZE), 3, 0);
-    mainlayout->addItem(new QSpacerItem(RFRAMESIZE, 0), 0, 2);
+    mainlayout->addItem(new QSpacerItem(RFRAMESIZE, BFRAMESIZE), 2, 2);
     mainlayout->addLayout(titlelayout, 1, 1);
     if (isPreview()) {
         mainlayout->addWidget(
         new QLabel(i18n("<b><center>Oxygen preview</center></b>"),
         widget()), 2, 1);
     } else {
-        mainlayout->addItem(new QSpacerItem(0, 0), 2, 1);
+        mainlayout->addItem(new QSpacerItem(0, 0), 1, 1);
     }
 
-    // the window should stretch
-    mainlayout->setRowStretch(2, 10);
-    mainlayout->setColumnStretch(1, 10);
 
     // setup titlebar buttons
     for (int n=0; n<ButtonTypeCount; n++) button[n] = 0;
     addButtons(titlelayout, options()->titleButtonsLeft());
     titlelayout->addItem(titlebar_);
     addButtons(titlelayout, options()->titleButtonsRight());
+
+    titlelayout->setSpacing(0);
+    titlelayout->setMargin(0);
+    mainlayout->setSpacing(0);
+    mainlayout->setMargin(0);
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -653,7 +654,7 @@ void OxygenClient::paintEvent(QPaintEvent*)
 
     // draw the titlebar+buttton background
     palette = options()->palette(KDecoration::ColorTitleBar, isActive());
-    painter.fillRect(QRect(0,TFRAMESIZE,width(), TITLESIZE+TFRAMESIZE), palette.background());
+    painter.fillRect(QRect(0,TFRAMESIZE,width(), TITLESIZE), palette.background());
 
     // draw title text
     QRect title(titlebar_->geometry());
