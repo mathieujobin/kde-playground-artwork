1c13231cb38f01b9d288bdc08a0a803efbf0119f
ruphy <ruphy@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-01-08 16:46:34 +0000

    now the script works
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/playground/artwork@621332 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- Oxygen/cursors/generate-cursor-theme.sh
+++ Oxygen/cursors/generate-cursor-theme.sh
@@ -10,13 +10,26 @@ Comment = The Oxygen mouse theme. Oxygenize your desktop!
 EOF
 
 #SVG -> PNG
+mkdir oxygen/tmp-sources
+cd oxygen/tmp-sources
 
+for icon in $(ls ../../svgs/*.svg); do
+inkscape --without-gui --export-png=$( echo $icon | sed s/......svgs.//|sed s/.svg// ).png --export-dpi=72 --export-background-opacity=0 --export-width=32 --export-height=32 $icon > /dev/null
+done
 
 #PNG+config file = cursor file
+#Warning, still in ./oxygen/tmp-sources
+cp ../../configs/* .
 
+for cursor in $(ls *.in); do
+xcursorgen $cursor ../cursors/$( echo $cursor | sed s/.in// )
+done
 
-tar -cf oxygen.tar oxygen
-gzip oxygen.tar
-rm oxygen.tar
+cd ../..
+rm -rf oxygen/tmp-sources
+
+#Package and remove all the temp stuff
+tar -cf oxygen$(date '+%F-%H-%M').tar oxygen
+gzip oxygen$(date '+%F-%H-%M').tar
 rm -rf oxygen
 
