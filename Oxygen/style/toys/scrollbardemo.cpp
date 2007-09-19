ef305f6cdcf25ff5cc899fa606200e619d9b5d31
mwoehlke <mwoehlke@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-09-19 01:57:20 +0000

    simple app containing some scroll bars, for testing scroll bars in oxygen style
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/playground/artwork@714254 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ Oxygen/style/toys/scrollbardemo.cpp
@@ -0,0 +1,34 @@
+#include <QtGui/QScrollBar>
+#include <KApplication>
+#include <KCmdLineArgs>
+#include <KAboutData>
+
+#include "ui_scrollbar.h"
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+public:
+    Widget(QWidget *parent=0) : QWidget(parent)
+    {
+        Ui::scrollbars ui;
+        ui.setupUi(this);
+    }
+};
+
+#include "scrollbartest.moc"
+
+int main(int argc, char **argv)
+{
+    KAboutData about("oxyscrollbartest", 0, ki18n("oxyscrollbartest"), "0.1",
+                     ki18n("Oxygen style scrollbar test application"),
+                     KAboutData::License_GPL, ki18n("(C) 2007 Matthew Woehlke"));
+    about.addAuthor( ki18n("Matthew Woehlke"), KLocalizedString(), "mw_triad@users.sourceforge.net" );
+    KCmdLineArgs::init(argc, argv, &about);
+    KApplication app;
+
+    Widget w;
+    w.show();
+
+    return app.exec();
+}
