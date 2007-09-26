50ece0e34c0a2cfa0e4a80a3f2c2de4f8479f059
mwoehlke <mwoehlke@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-09-26 22:24:54 +0000

    make luebking's uidemo into a KDE app, and add it somewhere more useful to me :-)
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/playground/artwork@717470 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ Oxygen/style/toys/uidemo.cpp
@@ -0,0 +1,42 @@
+/* This file is NOT part of KDE ;)
+   Copyright (C) 2007 Thomas Lübking <thomas.luebking@web.de>
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include <QtGui/QScrollBar>
+#include <KApplication>
+#include <KCmdLineArgs>
+#include <KAboutData>
+
+#include "ui_uidemo.h"
+
+int main(int argc, char **argv)
+{
+    KAboutData about("uidemo", 0, ki18n("uidemo"), "0.1",
+                     ki18n("uidemo"),
+                     KAboutData::License_GPL, ki18n("Copyright 2007 Thomas Lübking"));
+    about.addAuthor( ki18n("Thomas Lübking"), KLocalizedString(), "thomas.luebking@web.de" );
+    KCmdLineArgs::init(argc, argv, &about);
+    KApplication app;
+
+    QDialog *window = new QDialog;
+    Ui::Dialog ui;
+    ui.setupUi(window);
+    window->show();
+
+    return app.exec();
+}
