599f3aef7359035f31607fc004de1a85600453e3
sitter <sitter@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-12-30 23:31:27 +0000

    * Added close buttons * Copy some more icons using the update_icons script
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/playground/artwork@754923 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- Oxygen/firefox/update_icons.sh
+++ Oxygen/firefox/update_icons.sh
@@ -5,15 +5,26 @@ ICONPATH="../../../../KDE/kdebase/runtime/pics/oxygen"
 icon="16x16/status/dialog-warning.png"
 cp ${ICONPATH}/${icon} Oxygen/browser/safebrowsing/warning16x16.png
 cp ${ICONPATH}/${icon} Oxygen/mozapps/update/warning.gif
+cp ${ICONPATH}/${icon} Oxygen/global/console/bullet-warning.png
 
-cp ${ICONPATH}/22x22/status/dialog-warning.png Oxygen/browser/safebrowsing/warning24x24.png
-
-cp ${ICONPATH}/16x16/places/user-identity.png Oxygen/mozapps/profile/profileicon.png
+icon="16x16/status/dialog-information.png"
+cp ${ICONPATH}/${icon} Oxygen/global/console/bullet-question.png
+cp ${ICONPATH}/${icon} Oxygen/mozapps/extensions/question.png
 
 icon="16x16/actions/rss.png"
 cp ${ICONPATH}/${icon} Oxygen/browser/feeds/feedIcon16.png
 cp ${ICONPATH}/${icon} Oxygen/browser/page-livemarks.png
 
+cp ${ICONPATH}/22x22/status/dialog-warning.png Oxygen/browser/safebrowsing/warning24x24.png
+
+cp ${ICONPATH}/16x16/places/user-identity.png Oxygen/mozapps/profile/profileicon.png
+
 cp ${ICONPATH}/32x32/actions/rss.png Oxygen/browser/feeds/feedIcon.png
 
 cp ${ICONPATH}/32x32/apps/preferences-desktop-theme.png Oxygen/mozapps/extensions/themeGeneric.png
+
+cp ${ICONPATH}/16x16/actions/dialog-close.png Oxygen/browser/safebrowsing/close16x16.png
+
+cp ${ICONPATH}/16x16/status/dialog-error.png Oxygen/global/console/bullet-error.png
+
+cp ${ICONPATH}/32x32/status/dialog-information.png Oxygen/global/icons/Question.png
