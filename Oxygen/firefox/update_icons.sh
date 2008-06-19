3ea93f111b368f87e92158a5e23f1c157aacc613
sitter <sitter@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-06-19 22:33:44 +0000

    Welcome to Firefox 3 :-D First public release is probably going to be next week, so read the README and give the theme a testride. It replaces (almost) all GTK/Tango icons ... < 10 are missing oxygenish replacements (no really important ones though)
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/playground/artwork@822365 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- Oxygen/firefox/update_icons.sh
+++ Oxygen/firefox/update_icons.sh
@@ -2,32 +2,188 @@
 
 ICONPATH="../../../../KDE/kdebase/runtime/pics/oxygen"
 
-icon="16x16/status/dialog-warning.png"
-cp ${ICONPATH}/${icon} Oxygen/browser/safebrowsing/warning16x16.png
-cp ${ICONPATH}/${icon} Oxygen/mozapps/update/warning.gif
-cp ${ICONPATH}/${icon} Oxygen/global/console/bullet-warning.png
+icon="16x16/status/security-high.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/Secure.png
+
+icon="16x16/status/security-low.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/Security-broken.png
+
+icon="16x16/actions/edit-find.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/Search-glass.png
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/Search-glass-rtl.png
+cp ${ICONPATH}/${icon} oxygen/chrome/mozapps/extensions/searchIcons.png
+
+icon="16x16/mimetypes/application-rss+xml.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/feeds/feedIcon16.png
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/feeds/audioFeedIcon16.png
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/feeds/videoFeedIcon16.png
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/places/livemark-item.png
+
+icon="32x32/mimetypes/application-rss+xml.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/feeds/feedIcon.png
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/feeds/audioFeedIcon.png
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/feeds/videoFeedIcon.png
+
+icon="16x16/actions/configure.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/places/wrench.png
+
+icon="16x16/actions/feed-subscribe.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/places/tag.png
+
+icon="16x16/actions/rating.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/places/pageStarred.png
+
+icon="48x48/actions/rating.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/places/starred48.png
+
+icon="16x16/actions/view-pim-calendar.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/places/calendar.png
+
+icon="16x16/actions/bookmark-toolbar.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/places/bookmarksToolbar.png
+
+
+icon="16x16/actions/view-pim-mail.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/preferences/mail.png
+
+# TODO: 13x13
+icon="16x16/actions/arrow-left.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/Go-arrow-rtl.png
+
+icon="16x16/actions/arrow-right.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/Go-arrow.png
+
+icon="16x16/apps/preferences-web-browser-adblock.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/Info.png
+
+icon="16x16/actions/bookmark-new-list.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/places/bookmarksMenu.png
+
+icon="16x16/actions/services.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/places/unsortedBookmarks.png
+
+icon="16x16/apps/preferences-desktop-default-applications.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/preferences/application.png
+
+icon="16x16/actions/help-contents.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/browser/preferences/alwaysAsk.png
+
+
+
+
+
+
+icon="16x16/status/dialog-error.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/error-16.png
+
+icon="22x22/status/dialog-error.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/error-24.png
+
+icon="32x32/status/dialog-error.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/Error.png
+
+icon="48x48/status/dialog-error.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/error-48.png
+
+icon="64x64/status/dialog-error.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/error-64.png
 
 icon="16x16/status/dialog-information.png"
-cp ${ICONPATH}/${icon} Oxygen/global/console/bullet-question.png
-cp ${ICONPATH}/${icon} Oxygen/mozapps/extensions/question.png
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/information-16.png
 
-icon="16x16/actions/rss.png"
-cp ${ICONPATH}/${icon} Oxygen/browser/feeds/feedIcon16.png
-cp ${ICONPATH}/${icon} Oxygen/browser/page-livemarks.png
+icon="22x22/status/dialog-information.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/information-24.png
 
-cp ${ICONPATH}/22x22/status/dialog-warning.png Oxygen/browser/safebrowsing/warning24x24.png
+icon="48x48/status/dialog-information.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/information-48.png
 
-cp ${ICONPATH}/16x16/places/user-identity.png Oxygen/mozapps/profile/profileicon.png
+icon="64x64/status/dialog-information.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/information-64.png
 
-cp ${ICONPATH}/32x32/actions/rss.png Oxygen/browser/feeds/feedIcon.png
+icon="16x16/status/dialog-warning.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/warning-16.png
 
-cp ${ICONPATH}/32x32/apps/preferences-desktop-theme.png Oxygen/mozapps/extensions/themeGeneric.png
+icon="22x22/status/dialog-warning.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/warning-24.png
 
-cp ${ICONPATH}/16x16/actions/dialog-close.png Oxygen/browser/safebrowsing/close16x16.png
+icon="32x32/status/dialog-warning.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/Warning.png
 
-cp ${ICONPATH}/16x16/status/dialog-error.png Oxygen/global/console/bullet-error.png
+icon="48x48/status/dialog-warning.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/warning-48.png
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/warning-large.png
 
-cp ${ICONPATH}/32x32/status/dialog-information.png Oxygen/global/icons/Question.png
+icon="64x64/status/dialog-warning.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/icons/warning-64.png
 
-# cp ${ICONPATH}/32x32s/actions/edit-clear.png Oxygen/mozapps/downloads/downloadCleanup.png
-# echo "edit Oxygen/mozapps/downloads/downloadCleanupDisabled.png !!!!"
+icon="16x16/places/folder.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/dirListing/folder.png
+
+icon="16x16/devices/drive-harddisk.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/dirListing/local.png
+
+icon="16x16/places/network-server.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/dirListing/remote.png
+
+icon="16x16/actions/go-up.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/global/dirListing/up.png
+
+
+
+icon="32x32/apps/preferences-desktop-theme.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/mozapps/extensions/themeGeneric.png
+
+
+
+
+
+icon="16x16/mimetypes/text-html.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/mozapps/places/defaultFavicon.png
+
+icon="16x16/actions/feed-subscribe.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/mozapps/places/tagContainerIcon.png
+
+icon="16x16/apps/preferences-plugin.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/mozapps/update/update.png
+
+# TODO: gif
+# icon="16x16/status/dialog-warning.png"
+# cp ${ICONPATH}/${icon} oxygen/chrome/mozapps/update/warning.png
+
+icon="32x32/apps/preferences-plugin.png"
+cp ${ICONPATH}/${icon} oxygen/chrome/mozapps/xpinstall/xpinstallItemGeneric.png
+
+
+
+
+
+
+
+# icon="16x16/status/dialog-warning.png"
+# cp ${ICONPATH}/${icon} Oxygen/browser/safebrowsing/warning16x16.png
+# cp ${ICONPATH}/${icon} Oxygen/mozapps/update/warning.gif
+# cp ${ICONPATH}/${icon} Oxygen/global/console/bullet-warning.png
+
+# icon="16x16/status/dialog-information.png"
+# cp ${ICONPATH}/${icon} Oxygen/global/console/bullet-question.png
+# cp ${ICONPATH}/${icon} Oxygen/mozapps/extensions/question.png
+#
+# 
+# cp ${ICONPATH}/22x22/status/dialog-warning.png Oxygen/browser/safebrowsing/warning24x24.png
+# 
+# cp ${ICONPATH}/16x16/places/user-identity.png Oxygen/mozapps/profile/profileicon.png
+# 
+# cp ${ICONPATH}/32x32/actions/rss.png Oxygen/browser/feeds/feedIcon.png
+# 
+# cp ${ICONPATH}/32x32/apps/preferences-desktop-theme.png Oxygen/mozapps/extensions/themeGeneric.png
+# 
+# cp ${ICONPATH}/16x16/actions/dialog-close.png Oxygen/browser/safebrowsing/close16x16.png
+# 
+# cp ${ICONPATH}/16x16/status/dialog-error.png Oxygen/global/console/bullet-error.png
+# 
+# cp ${ICONPATH}/32x32/status/dialog-information.png Oxygen/global/icons/Question.png
+# 
+# cp ${ICONPATH}/16x16/apps/preferences-plugin.png Oxygen/chrome/mozapps/update/update.png
+# 
+# # cp ${ICONPATH}/32x32s/actions/edit-clear.png Oxygen/mozapps/downloads/downloadCleanup.png
+# # echo "edit Oxygen/mozapps/downloads/downloadCleanupDisabled.png !!!!"
