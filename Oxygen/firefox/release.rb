3bf397821c27668c905c8489bb09b7cb425f4c8d
sitter <sitter@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-07-01 17:45:01 +0000

    * general cleanup * remove oxygen.jar * add release script * prepare for 1.0 release
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/playground/artwork@826867 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ Oxygen/firefox/release.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+#
+# Generates a release tarball for the firefox theme
+#
+# Copyright (C) 2007-2008 Harald Sitter <harald@oxygen-icons.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License or (at your option) version 3 or any later version
+# accepted by the membership of KDE e.V. (or its successor approved
+# by the membership of KDE e.V.), which shall act as a proxy 
+# defined in Section 14 of version 3 of the license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'fileutils'
+
+if $*[0] == nil
+    puts("please provide a version number")
+    exit 1
+else
+    version = $*[0]
+end
+
+releasedir = "release-#{version}"
+system("svn export Oxygen #{releasedir}")
+
+Dir.chdir(releasedir)
+
+file = File.new( "install.rdf", File::RDWR )
+str = file.read()
+file.rewind()
+file.truncate( 0 )
+str.sub!( /<em:version>.*<\/em:version>/, "<em:version>#{version}</em:version>" )
+file << str
+file.close()
+
+system("zip -r ../Oxygen-#{version}.jar *")
+Dir.chdir("../")
+
+FileUtils.rm_r(releasedir)
+
+system("sha1sum Oxygen-#{version}.jar")
+
+exit 0
--- Oxygen/firefox/update_icons.sh
+++ Oxygen/firefox/update_icons.sh
@@ -154,36 +154,3 @@ icon="32x32/apps/preferences-plugin.png"
 cp ${ICONPATH}/${icon} oxygen/chrome/mozapps/xpinstall/xpinstallItemGeneric.png
 
 
-
-
-
-
-
-# icon="16x16/status/dialog-warning.png"
-# cp ${ICONPATH}/${icon} Oxygen/browser/safebrowsing/warning16x16.png
-# cp ${ICONPATH}/${icon} Oxygen/mozapps/update/warning.gif
-# cp ${ICONPATH}/${icon} Oxygen/global/console/bullet-warning.png
-
-# icon="16x16/status/dialog-information.png"
-# cp ${ICONPATH}/${icon} Oxygen/global/console/bullet-question.png
-# cp ${ICONPATH}/${icon} Oxygen/mozapps/extensions/question.png
-#
-# 
-# cp ${ICONPATH}/22x22/status/dialog-warning.png Oxygen/browser/safebrowsing/warning24x24.png
-# 
-# cp ${ICONPATH}/16x16/places/user-identity.png Oxygen/mozapps/profile/profileicon.png
-# 
-# cp ${ICONPATH}/32x32/actions/rss.png Oxygen/browser/feeds/feedIcon.png
-# 
-# cp ${ICONPATH}/32x32/apps/preferences-desktop-theme.png Oxygen/mozapps/extensions/themeGeneric.png
-# 
-# cp ${ICONPATH}/16x16/actions/dialog-close.png Oxygen/browser/safebrowsing/close16x16.png
-# 
-# cp ${ICONPATH}/16x16/status/dialog-error.png Oxygen/global/console/bullet-error.png
-# 
-# cp ${ICONPATH}/32x32/status/dialog-information.png Oxygen/global/icons/Question.png
-# 
-# cp ${ICONPATH}/16x16/apps/preferences-plugin.png Oxygen/chrome/mozapps/update/update.png
-# 
-# # cp ${ICONPATH}/32x32s/actions/edit-clear.png Oxygen/mozapps/downloads/downloadCleanup.png
-# # echo "edit Oxygen/mozapps/downloads/downloadCleanupDisabled.png !!!!"
