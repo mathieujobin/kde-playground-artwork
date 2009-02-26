c065e58184065dbb4cd8ed9c2de7c64c83ab1d7f
ruphy <ruphy@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-02-26 10:54:24 +0000

    added TODO file
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/playground/artwork@932292 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- Oxygen/cursors/src/config.cmake
+++ Oxygen/cursors/src/config.cmake
@@ -3,5 +3,4 @@ file(GLOB CONFIGS config/*.in)
 
 set(SVGDIR ${CMAKE_SOURCE_DIR}/svg)
 set(CONFIGDIR ${CMAKE_SOURCE_DIR}/config)
-set(MAKE_CONFIG ${CMAKE_SOURCE_DIR}/make_config.cmake)
-set(MAKE_SVG ${CMAKE_SOURCE_DIR}/make_svg.cmake)
+set(MAKE_SVG ${CMAKE_SOURCE_DIR}/make_svg)
--- Oxygen/cursors/src/cursors.cmake
+++ Oxygen/cursors/src/cursors.cmake
@@ -1,10 +1,9 @@
 macro(add_cursor cursor color theme dpi)
     add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
                        DEPENDS ${MAKE_SVG} ${CMAKE_CURRENT_SOURCE_DIR}/colors.in ${SVGDIR}/${cursor}.svg
-                       COMMAND ${CMAKE_COMMAND} -Dconfig=${CMAKE_CURRENT_SOURCE_DIR}/colors.in
-                                                -Dinput=${SVGDIR}/${cursor}.svg
-                                                -Doutput=${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
-                                                -P ${MAKE_SVG}
+                       COMMAND ${MAKE_SVG} ${CMAKE_CURRENT_SOURCE_DIR}/colors.in
+                                           ${SVGDIR}/${cursor}.svg
+                                           ${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
                       )
     add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/png/${cursor}.png
                        DEPENDS ${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
@@ -14,22 +13,16 @@ macro(add_cursor cursor color theme dpi)
                       )
 endmacro(add_cursor)
 
-macro(add_x_cursor theme cursor dpi)
+macro(add_x_cursor theme cursor)
     set(inputs)
     foreach(png ${${cursor}_inputs})
         list(APPEND inputs ${CMAKE_BINARY_DIR}/oxy-${theme}/png/${png})
     endforeach(png)
-    add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/config/${cursor}.in
-                       DEPENDS ${MAKE_CONFIG} ${CONFIGDIR}/${cursor}.in
-                       COMMAND ${CMAKE_COMMAND} -Dconfig=${CONFIGDIR}/${cursor}.in
-                                                -Doutput=${CMAKE_BINARY_DIR}/oxy-${theme}/config/${cursor}.in
-                                                -Ddpi=${dpi}
-                                                -P ${MAKE_CONFIG}
-                      )
+    # TODO rewrite input for dpi
     add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/cursors/${cursor}
-                       DEPENDS ${inputs} ${CMAKE_BINARY_DIR}/oxy-${theme}/config/${cursor}.in
+                       DEPENDS ${inputs}
                        COMMAND ${XCURSORGEN} -p ${CMAKE_BINARY_DIR}/oxy-${theme}/png
-                                             ${CMAKE_BINARY_DIR}/oxy-${theme}/config/${cursor}.in
+                                             ${CONFIGDIR}/${cursor}.in
                                              ${CMAKE_BINARY_DIR}/oxy-${theme}/cursors/${cursor}
                       )
 endmacro(add_x_cursor)
@@ -38,7 +31,6 @@ file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/packages)
 macro(add_theme color theme dpi)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/oxy-${theme}/png)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/oxy-${theme}/svg)
-    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/oxy-${theme}/config)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/oxy-${theme}/cursors)
     set(${theme}_cursors)
     foreach(svg ${SVGS})
@@ -47,7 +39,7 @@ macro(add_theme color theme dpi)
         add_cursor(${cursor} ${color} ${theme} ${dpi})
     endforeach(svg)
     foreach(cursor ${CURSORS})
-        add_x_cursor(${theme} ${cursor} ${dpi})
+        add_x_cursor(${theme} ${cursor})
         list(APPEND ${theme}_cursors ${CMAKE_BINARY_DIR}/oxy-${theme}/cursors/${cursor})
     endforeach(cursor)
     add_custom_target(theme-${theme} ALL DEPENDS ${${theme}_cursors})
